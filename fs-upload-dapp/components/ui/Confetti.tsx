29c4007ee2ed53b3e62431f9f6ca256911c69606
monyverse <manx393@gmail.com>
2025-07-12 05:11:22 +0200

    rm demos
    Signed-off-by: monyverse <manx393@gmail.com>

--- fs-upload-dapp/components/ui/Confetti.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import ReactConfetti from "react-confetti";
-
-export default function Confetti(
-  props: React.ComponentProps<typeof ReactConfetti>
-) {
-  if (typeof window === "undefined") return null;
-  return <ReactConfetti {...props} />;
-}
--- fs-upload-dapp/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./constants";
-export * from "./calculateStorageMetrics";
-export * from "./getProofset";
-export * from "./preflightCheck";
