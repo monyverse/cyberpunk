3b09ffc66d92c5eb92fe33ec9cb98ee0121c9d43
monyverse <manx393@gmail.com>
2025-07-16 04:45:07 +0200

    fix appkit providers
    Signed-off-by: monyverse <manx393@gmail.com>

--- config/appkit.ts
+++ config/appkit.ts
@@ -1,6 +1,7 @@
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
 import { filecoin, filecoinCalibration, mainnet, polygon } from '@reown/appkit/networks';
 import type { AppKitNetwork } from '@reown/appkit/networks';
+import { createAppKit } from '@reown/appkit/react';
 
 export const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || '865c931386843631a84e0461df3b26fa';
 
@@ -11,6 +12,13 @@ if (!projectId) {
 export const networks: [AppKitNetwork, ...AppKitNetwork[]] = [filecoin, filecoinCalibration, mainnet, polygon];
 
 export const wagmiAdapter = new WagmiAdapter({
+  projectId,
+  networks,
+  ssr: true,
+});
+
+export const appKit = createAppKit({
+  adapters: [wagmiAdapter],
   projectId,
   networks,
   ssr: true,
